f1b433c2f5db03b9cc8a20d7e0e5715b8be7e52a
riri <claraligz@gmail.com>
2020-02-27 22:17:10 +0800

    add exercise04

---
+++ src/EscapeSequencesComments.java
@@ -0,0 +1,18 @@
+public class EscapeSequencesComments {
+    public static void main(String[] args){
+    //This exercise demonstrates escape sequencs & comments (like these)!
+        System.out.print("Learn\tJava\n\tthe\nHard\tWay\n\n");
+        System.out.print("\tLearn Java the \"Hard\" Way!\n");
+        //System.out.frimp("Learn Java the Hard Way");
+        System.out.print("Hello\n");
+        System.out.print("Jello\by\n");
+        /* The quick brown fox jumped over a lazy dog.
+           Quick wafting zephyrs vex bold Jim. */
+        System.out/* testing */ .println("Hard to believe, eh?");
+        System.out.println("Surprised? /* abcd */ Or waht did you expect?");
+        System.out.println("\\ // -=- \\ //");
+        System.out.println("\\\\ \\\\\\ \\\\\\\\ "); // it takes 2 to make 1
+        System.out.print("I hope you understand \"escape sequences\" now.\n");
+        // and comments. :)
+    }
+}
---
+++ src/Exercise04.md
@@ -0,0 +1,24 @@
+# Excerise03
+
+## Study Notes
+
+遇到的问题：
+
+- 输入`javac PrintingChoices`后报错`error: Class names, 'PrintingChoices', are only accepted if annotation processing is explicitly requested
+  1 error`. The right command should be `javac PrintingChoices.java`
+
+  
+
+## Study Drills
+
+1. Add a printing statement at the end of the code to display the sentence “I am learning  Java the Hard Way!” But break it up like line 19 so that you only have two or three words in each String and plus signs between. Make sure the spaces get included!
+
+```java
+System.out.println("I am" + " " + "learning" + " " + "Java the Hard" + " " + "Way!" );
+```
+
+
+
+## logging
+
+2020-02-27 13:29:58 init
